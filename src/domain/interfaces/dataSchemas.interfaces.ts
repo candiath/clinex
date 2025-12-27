555759e23613378ad81490f7f265ccc3343ae278
Nath <55251304+candiath@users.noreply.github.com>
2025-12-27 13:31:21 -0300

    refactor(schemas): enhance validation messages in data schemas for better clarity

--- src/domain/interfaces/dataSchemas.interfaces.ts
+++ src/domain/interfaces/dataSchemas.interfaces.ts
@@ -1,13 +1,28 @@
 import * as z from "zod";
 import { DoctorSpecialty } from "../types/doctorSpecialty.type";
 
-export const EntityIDSchema = z.coerce.number().int().positive().nonoptional();
-export const EmailSchema = z.email().nonoptional();
+export const EntityIDSchema = z.coerce
+  .number()
+  .int()
+  .positive("ID should be greater than 0")
+  .nonoptional();
+export const EmailSchema = z.email("Missing or invalid email").nonoptional();
 export const MedicalSpecialtySchema = z.enum(DoctorSpecialty).nonoptional();
-export const FutureDateSchema = z.coerce.date().nonoptional();
-export const PhoneSchema = z.string().trim().regex(/^[0-9+()\-\s]{7,20}$/, "Invalid phone number");
-export const NameSchema = z.string().min(1).nonoptional();
-export const BirthDateSchema = z.coerce.date().max(new Date()).nonoptional();
+export const FutureDateSchema = z.coerce
+  .date("Date cannot be in the past")
+  .nonoptional();
+export const PhoneSchema = z
+  .string("Phone is required or was provided in a wrong format")
+  .trim()
+  .regex(/^[0-9+()\-\s]{7,20}$/, "Invalid phone number");
+export const NameSchema = z
+  .string("Name is required")
+  .min(3, "Name is too short")
+  .nonoptional();
+export const BirthDateSchema = z.coerce
+  .date("Invalid birth date")
+  .max(new Date(), "Birth date cannot be in the future")
+  .nonoptional();
 export const DniSchema = z.coerce
   .number()
   .int()
@@ -16,8 +31,19 @@ export const DniSchema = z.coerce
   .max(99_999_999)
   .nonoptional()
   .transform((val) => val.toString());
-export const SexSchema = z.enum(["male", "female", "other", "prefer not to say"]).nonoptional();
-export const AppointmentStatusSchema = z.enum(["scheduled", "completed", "canceled"]).nonoptional();
+export const SexSchema = z
+  .enum(["male", "female", "other", "prefer not to say"])
+  .nonoptional();
+export const AppointmentStatusSchema = z
+  .enum([
+    "SCHEDULED",
+    "CONFIRMED",
+    "IN_PROGRESS",
+    "COMPLETED",
+    "CANCELLED",
+    "NO_SHOW",
+  ])
+  .nonoptional();
 
 export const AppointmentDataSchema = z.object({
   date: FutureDateSchema,
