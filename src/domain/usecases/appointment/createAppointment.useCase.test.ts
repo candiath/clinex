ccb1e373348dbc7ac813beb014efb01e4d6dcebd
Nath <55251304+candiath@users.noreply.github.com>
2026-01-11 20:13:11 -0300

    test(appointments): update appointment test to use correct dateTime field and enable error handling test

--- src/domain/usecases/appointment/createAppointment.useCase.test.ts
+++ src/domain/usecases/appointment/createAppointment.useCase.test.ts
@@ -1,10 +1,8 @@
-import { Appointment } from "../../entities/appointment.entity";
 import { AppointmentRepositoryImplementation } from "../../../infrastructure/repositories/appointment.repository.implementation";
 import { AppointmentDatasource } from "../../datasources/appointment.datasource";
 import { CustomError } from "../../errors/customError";
 import { CreateAppointmentUseCase } from "./createAppointment.useCase";
 import { EntityID } from "../../valueObjects/entityID";
-import { AppointmentStatus } from "../../valueObjects/appointmentStatus";
 import { createAppointmentInput } from "../../interfaces/appointment.interfaces";
 
 jest.mock(
@@ -38,7 +36,7 @@ describe("CreateAppointmentUseCase", () => {
   const VALID_APPOINTMENT_DATA: createAppointmentInput = {
     patientId: VALID_PATIENT_ID_1,
     doctorId: VALID_DOCTOR_ID_1,
-    date: new Date("2025-12-31 12:34:56"),
+    dateTime: new Date("3025-12-31 12:34:56"),
     status: "SCHEDULED",
   };
 
@@ -48,7 +46,7 @@ describe("CreateAppointmentUseCase", () => {
   });
 
   describe("Error handling", () => {
-    it("Should return an error when repository fails", async () => {
+    it.only("Should return an error when repository fails", async () => {
       mockRepository.create.mockRejectedValue(
         CustomError.internalServerError("MOCKDatabase error")
       );
